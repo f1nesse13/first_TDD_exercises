5be6d4c37c8059e37d0ba1f17bd25975eeff183d
f1nesse13 <philly.teg@gmail.com>
2019-01-29 12:56:03 -0500

    additional methods/tests for Game

--- lib/tdd_methods.rb
+++ lib/tdd_methods.rb
@@ -27,5 +27,70 @@ class Array
     transposed
   end
 
+  def stock_picker
+    best_days = []
+    profit = 0
+    cur_profit = 0
+    self.each_with_index do |ele1, i|
+      self.each_with_index do |ele2, x|
+        profit = ele2 - ele1
+        if profit > cur_profit && x > i
+          cur_profit = profit
+          best_days = [i, x]
+        end
+      end
+    end
+    best_days
+  end
+end
+
+class Towers
+  attr_accessor :board
+    def initialize
+      @board = [[3,2,1], [], []]
+    end
+
+    def valid_move?(start_pos, end_pos)
+      return false unless [start_pos, end_pos].all? { |i| i.between?(0,2) }
+      !@board[start_pos].empty? &&
+      (@board[end_pos].empty? || @board[end_pos].last >
+      @board[start_pos].last)
+    end
+
+    def move(start_pos, end_pos)
+      raise "that stack is empty" if @board[start_pos].empty?
+      raise "can't move onto a smaller disc" if !valid_move?(start_pos, end_pos)
+      @board[end_pos] << @board[start_pos].pop
+    end
+
+    def play
+      until won?
+          render
+          puts "Pick a tower to move from"
+          start_pos = gets.chomp.to_i
+          puts "Where do you want to place the disc?"
+          end_pos = gets.chomp.to_i
+
+          if valid_move?(start_pos, end_pos)
+            move(start_pos, end_pos)
+          else
+            puts "Invalid move"
+          end
+        end
+      puts "Congratulations!"
+    end
+
+    def won?
+      @board[0].empty? && @board[1..2].any?(&:empty?)
+    end
+
+    def render
+      system("clear")
+    board.each_with_index do |row, i|
+      puts "Tower #{i}: #{row.join(" ")}"
+    end
+    end
+
 
 end
+# Towers.new.play
--- spec/tdd_methods_spec.rb
+++ spec/tdd_methods_spec.rb
@@ -23,4 +23,53 @@ describe Array do
         expect(arr2.my_transpose).to eq([[0, 1, 2],[3, 4, 5],[6, 7, 8]])        
       end
     end
+
+    describe '#stock_picker' do
+      it 'will return the best days to buy and sell for max profit' do
+        arr1 = [1.00, 2.00, 3.00, 4.00, 5.00]
+        arr2 = [4.00, 3.00, 2.00, 1.00, 5.00]
+        expect(arr1.stock_picker).to eq([0, 4])
+        expect(arr2.stock_picker).to eq([3, 4])
+
+     end
+    end
+end
+
+describe Towers do
+  subject(:tower) { Towers.new }
+  describe '#initialize' do
+    it 'initializes with a new game board' do
+      expect(tower.board).to eq([[3,2,1], [], []])
+    end
+  end
+
+  describe '#move' do
+    it 'moves the disc into the correct stack' do
+      tower.move(0, 2)
+      expect(tower.board).to eq([[3,2], [], [1]])
+    end
+    it 'throws a error if stack has no discs' do
+      expect { tower.move(2,1) }.to raise_error('that stack is empty')
+    end
+
+    it 'throws an error if you try to place a bigger disc onto a smaller' do
+      tower.move(0, 2)
+      expect {tower.move(0,2)}.to raise_error("can't move onto a smaller disc")
+    end
+  end
+
+  describe '#won?' do
+
+    it 'is won when all discs are moved onto another tower' do
+      tower.move(0, 1)
+      tower.move(0, 2)
+      tower.move(1, 2)
+      tower.move(0, 1)
+      tower.move(2, 0)
+      tower.move(2, 1)
+      tower.move(0, 1)
+
+      expect(tower).to be_won
+    end
+  end
 end
